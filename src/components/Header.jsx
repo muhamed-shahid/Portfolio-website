656307fca00bb9704caa6b18df8aa27d9d950963
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-10-29 15:25:26 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container">
+            {/* logo */}
+            <a href="#">Shahid</a>
+
+            {/* Mobile menu */}
+            <nav className="">
+                <ul>
+                    {}
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
