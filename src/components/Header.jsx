ea70ea6c1f477b38561c05d1a5c722de244d5177
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-10-30 15:17:02 +0530

    setting header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
+import { navItems } from '../constant/data'
 
 const Header = () => {
   return (
-    <header>
-        <div className="container">
+    <header className='header'>
+        <div className="container flex items-center justify-between">
             {/* logo */}
-            <a href="#">Shahid</a>
+            <a href="#" className='text-3xl font-semibold'>Shahid</a>
 
             {/* Mobile menu */}
-            <nav className="">
-                <ul>
-                    {}
+            <nav className='lg:hidden absolute top-0 left-0 
+            bg-secondary-clr/70 w-full backdrop-blur-2xl max-h-0
+            overflow-hidden mt-20 flex items-center justify-center z-20'>
+                <ul className='text-center space-y-6 p-7'>
+                    {navItems.map(item=>(
+                        <li key={item.id}>
+                            <a href={item.href}
+                             className='text-lg y-3 relative after:absolute
+                              after:bottom-0 after:left-0
+                              after:bg-white after:w-0 after:h-0.5
+                               after:rounded hover:after:w-full after:transition-[width] duration-300 '>{item.label}</a>
+                        </li>
+                    ))}
                 </ul>
             </nav>
+            {/* lg menu */}
+             <ul className='hidden lg:flex gap-8 items-center'>
+                    {navItems.map(item=>(
+                        <li key={item.id}>
+                            <a href={item.href} className='text-lg y-3 relative after:absolute
+                              after:bottom-0 after:left-0
+                              after:bg-white after:w-0 after:h-0.5
+                               after:rounded hover:after:w-full after:transition-[width] duration-300 '>{item.label}</a>
+                        </li>
+                    ))}
+                </ul>
+                {/* menu btn */}
+                <button className="lg:hidden relative h-7 w-8 flex items-center justify-center">
+                    <span className=""></span>
+                    <span className=""></span>
+                </button>
         </div>
     </header>
   )
