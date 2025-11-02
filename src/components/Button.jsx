c88c3843c3b69fc6b36906f5a50dfb09ecdc0b41
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-02 11:10:54 +0530

    setting buttons in hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
+import { RiDownloadLine } from '@remixicon/react'
 import React from 'react'
 
-const Button = () => {
+const Button = ({label,classes, icon}) => {
   return (
-    <div>Button</div>
+    <button className={`${classes} flex items-center gap-2`}>
+      {label}
+    {icon && (
+      <span>
+      <RiDownloadLine />
+      </span>
+    )}
+      </button>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,28 +4,28 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section>
-        <div className="container">
+    <section className='py-28 lg:pt-40'>
+        <div className="container flex flex-col items-center">
             {/* image */}
-            <figure>
+            <figure className='rounded-full overflow-hidden'>
                 <img src="/images/hero-img.png" alt="Muhamed Shahid"  width={452} height={452}/>
             </figure>
-            <div>
-              <h1>Hi,I'm Muhamed Shahid.</h1>
+            <div className='text-center mt-8'>
+              <h1 className='text-3xl lg:text-5xl mb-1.5 font-bold'>Hi,I'm Muhamed Shahid.</h1>
               <h2>Web Developer</h2>
-              <p>I'm Muhamed Shahid, a motivated frontend developer specializing in React. With a strong foundation in JavaScript and modern web technologies, I aim to design scalable and user-friendly interfaces. My portfolio project reflects my journey of learning, problem-solving, and building impactful web solutions.</p>
+              <p className='max-w-[820px] mx-auto mt-4 mb-6'>I'm Muhamed Shahid, a motivated web developer specializing in React. With a strong foundation in JavaScript and modern web technologies, I aim to design scalable and user-friendly interfaces. My portfolio project reflects my journey of learning, problem-solving, and building impactful web solutions.</p>
 
-              <div className="">
+              <div className="flex items-center justify-center gap-5">
                 {heroIcons.map(icon => (
-                  <a href="#" target='-blank'>
+                  <a href="#" target='-blank' className='hover:text-indigo-400 transition-colors'>
                     <icon.icon size={30}/>
                   </a>
                 ))}
               </div>
               {/* btn wrapper */}
-              <div className="">
-                <Button label="Download CV" classes=""/>
-                <Button label="Let's Collaborate" classes=""/>
+              <div className="mt-10 flex items-center justify-center flex-wrap gap-5">
+                <Button label="Download CV" classes="primary-btn"/>
+                <Button label="Let's Collaborate" classes="secondary-btn"/>
               </div>
             </div>
         
