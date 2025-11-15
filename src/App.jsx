1059d8d0e5bfaed53ebb1bde53a9dc6e423be2a5
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-15 10:27:43 +0530

    Making Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Experience/>
+      <Projects/>
     </main>
     </>
   )
---
+++ src/components/Projects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Carousel } from './ui/carousel'
+import { ProjectsItems } from '../constant/data'
+
+const Projects = () => {
+  return (
+    <section>
+        <div className="container">
+            {/* Title */}
+            <h2>Recent Projects</h2>
+            {/* Card wrapper */}
+            <div className='relative overflow-hidden w-full h-full py-20'>
+                <Carousel slides={ProjectsItems}/>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default Projects
