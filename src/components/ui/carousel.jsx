5f800f785e9b5f8a679a770295c79bfec9f798f4
muhamed-shahid <muhamedshahid7860@gmail.com>
2025-11-15 10:44:46 +0530

    Projects section completed

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -1,3 +1,4 @@
+import { RiArrowRightUpLongLine } from "@remixicon/react";
 import { RiArrowRightLongLine } from "@remixicon/react";
 import { useState, useRef, useId, useEffect } from "react";
 
@@ -53,7 +54,7 @@ const Slide = ({
     event.currentTarget.style.opacity = "1";
   };
 
-  const { src, button, title } = slide;
+  const { src, text, title } = slide;
 
   return (
     <div className="[perspective:1200px] [transform-style:preserve-3d]">
@@ -101,10 +102,11 @@ const Slide = ({
           <h2 className="text-lg md:text-2xl lg:text-4xl font-semibold  relative">
             {title}
           </h2>
+          <p className="bg-white/80 text-black p-3 rounded-lg">{text}</p>
           <div className="flex justify-center">
             <button
-              className="mt-6  px-4 py-2 w-fit mx-auto sm:text-sm text-black bg-white h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
-              {button}
+              className="mt-6  px-4 py-1.5 w-fit mx-auto sm:text-sm text-black bg-white hover:bg-white/80 h-12 border border-transparent text-xs flex justify-center items-center rounded-2xl hover:shadow-lg transition duration-200 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]">
+             <a href="#" target="_blank"><RiArrowRightUpLongLine/></a>
             </button>
           </div>
         </article>
